5ed25a8fe8eac0ff58869a4b412eed34e4ba8a8a
Rafie Mirza Ramadhan <rafiemirzar@gmail.com>
2023-05-30 00:57:08 +0700

    update database

--- basasunda.sql
+++ basasunda.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 15 Bulan Mei 2019 pada 10.06
--- Versi server: 10.1.36-MariaDB
--- Versi PHP: 5.6.38
+-- Generation Time: May 29, 2023 at 07:54 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,29 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
   `id` int(11) NOT NULL,
   `label` varchar(100) NOT NULL,
   `color` varchar(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `label`, `color`) VALUES
 (1, 'Numbers', 'FD8E09'),
 (2, 'Family Members', '379237'),
 (3, 'Colors', '8800A0'),
-(4, 'Phrases', '16AFCA');
+(4, 'Phrases', '16AFCA'),
+(5, 'Place', '1B9C85'),
+(6, 'Family', 'B70404');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `word`
+-- Table structure for table `word`
 --
 
 CREATE TABLE `word` (
@@ -56,83 +57,81 @@ CREATE TABLE `word` (
   `label` varchar(100) NOT NULL,
   `sunda` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Dumping data untuk tabel `word`
+-- Dumping data for table `word`
 --
 
 INSERT INTO `word` (`id`, `cat_id`, `label`, `sunda`, `image`) VALUES
-(1, 1, 'one', 'hiji', 'number_one.png'),
-(2, 1, 'two', 'dua', 'number_two.png'),
-(3, 1, 'three', 'tilu', 'number_three.png'),
-(4, 1, 'four', 'opat', 'number_four.png'),
-(5, 1, 'five', 'lima', 'number_five.png'),
-(6, 1, 'six', 'genep', 'number_six.png'),
-(7, 1, 'seven', 'tujuh', 'number_seven.png'),
-(8, 1, 'eight', 'dalapan', 'number_eight.png'),
-(9, 1, 'nine', 'salapan', 'number_nine.png'),
-(10, 1, 'ten', 'sapuluh', 'number_ten.png'),
-(11, 2, 'father', 'bapa', 'family_father.png'),
-(12, 2, 'mother', 'ema', 'family_mother.png'),
-(13, 2, 'son', 'putra', 'family_son.png'),
-(14, 2, 'daughter', 'putri', 'family_daughter.png'),
-(15, 2, 'older brother', 'lanceuk pamegeut', 'family_older_brother.png'),
-(16, 2, 'younger brother', 'adi pamegeut', 'family_younger_brother.png'),
-(17, 2, 'older sister', 'lanceuk isteri', 'family_older_sister.png'),
-(18, 2, 'younger sister', 'adi isteri', 'family_younger_sister.png'),
-(19, 2, 'grandmother', 'aki', 'family_grandmother.png'),
-(20, 2, 'grandfather', 'nini', 'family_grandfather.png'),
-(21, 3, 'red', 'beureum', 'color_red.png'),
-(22, 3, 'green', 'hejo', 'color_green.png'),
-(23, 3, 'brown', 'coklat', 'color_brown.png'),
-(24, 3, 'gray', 'kulawu', 'color_gray.png'),
-(25, 3, 'black', 'hideung', 'color_black.png'),
-(26, 3, 'white', 'bodas', 'color_white.png'),
-(27, 3, 'dusty yellow', 'koneng ngora', 'color_dusty_yellow.png'),
-(28, 3, 'mustard yellow', 'koneng kolot', 'color_mustard_yellow.png'),
-(29, 4, 'Where are you going?', 'Bade angkat kamana?', ''),
-(30, 4, 'What is your name?', 'Saha nami anjeun?', ''),
-(31, 4, 'My name is...', 'Nami abdi...', ''),
-(32, 4, 'How are you feeling?', 'Kumaha karaosna ku anjeun?', ''),
-(33, 4, 'I\'m feeling good.', 'Abdi ngaraos sae.', ''),
-(34, 4, 'Are you coming?', 'Anjeun bade dongkap?', ''),
-(35, 4, 'Yes, I\'m coming.', 'Sumuhun, abdi bade dongkap.', ''),
-(36, 4, 'I\'m coming.', 'Abdi dongkap.', ''),
-(37, 4, 'Let\'s go.', 'Hayu.', ''),
-(38, 4, 'Come here.', 'Kadieu.', '');
+(1, 1, 'One', 'Satu', 'number_One_1685280169.jpeg'),
+(2, 1, 'Two', 'Dua', 'number_Two_1685280564.jpeg'),
+(3, 1, 'Three', 'Tiga', 'number_Three_1685280580.jpeg'),
+(4, 1, 'Four', 'Empat', 'number_Four_1685280591.jpeg'),
+(5, 1, 'Five', 'Lima', 'number_Five_1685280925.jpeg'),
+(6, 1, 'Six', 'Enam', 'number_Six_1685280933.jpeg'),
+(7, 1, 'Seven', 'Tujuh', 'number_Seven_1685280961.jpeg'),
+(8, 1, 'Eight', 'Delapan', 'number_Eight_1685280986.jpeg'),
+(9, 1, 'Nine', 'Sembilan', 'number_Nine_1685281003.jpeg'),
+(10, 1, 'Ten', 'Sepuluh', 'number_Ten_1685281020.jpeg'),
+(11, 2, 'A', 'A', 'family_A_1685281405.jpeg'),
+(12, 2, 'B', 'B', 'family_B_1685281416.jpeg'),
+(13, 2, 'C', 'C', 'family_C_1685281440.jpeg'),
+(14, 2, 'D', 'D', 'family_D_1685281450.jpeg'),
+(15, 2, 'E', 'E', 'family_E_1685281459.jpeg'),
+(16, 2, 'F', 'F', 'family_F_1685281471.jpeg'),
+(17, 2, 'G', 'G', 'family_G_1685281481.jpeg'),
+(18, 2, 'H', 'H', 'family_H_1685281491.jpeg'),
+(19, 2, 'I', 'I', 'family_I_1685281500.jpeg'),
+(20, 2, 'J', 'J', 'family_J_1685281510.jpeg'),
+(21, 3, 'Scared', 'Takut', 'color_afraid_1668586537.jpg'),
+(22, 3, 'Love', 'Cinta', 'color_Love_1668586559.jpg'),
+(23, 3, 'Angry', 'Marah', 'color_Marah_1668586687.jpg'),
+(24, 3, 'Sad', 'Sedih', 'color_Sad_1668586697.jpg'),
+(50, 4, 'He/She', 'Dia', 'dia.png'),
+(51, 4, 'We', 'Kita', 'kita.png'),
+(52, 4, 'You', 'Kamu', 'kamu.png'),
+(53, 4, 'Us', 'Kami', 'kami.png'),
+(54, 4, 'They', 'Mereka', 'mereka.png'),
+(55, 4, 'Me', 'Saya', 'saya.png'),
+(56, 5, 'Bank', 'Bank', 'bank.png'),
+(57, 5, 'Classroom', 'Ruang Kelas', 'kelas.png'),
+(58, 5, 'Home', 'Rumah', 'rumah.png'),
+(59, 5, 'Market', 'Pasar', 'pasar.png'),
+(60, 5, 'School', 'Sekolah', 'sekolah.png'),
+(61, 5, 'Park', 'Taman', 'taman.png');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `word`
+-- Indexes for table `word`
 --
 ALTER TABLE `word`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT untuk tabel `word`
+-- AUTO_INCREMENT for table `word`
 --
 ALTER TABLE `word`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
